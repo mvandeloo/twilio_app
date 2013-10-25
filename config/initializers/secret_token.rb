03c42a4284839eac145a29cc9dd1904dce52183a
mvandeloo <martin.vandeloo@googlemail.com>
2013-10-25 10:34:04 +0100

    initial app setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwilioApp::Application.config.secret_key_base = '32c37f1997403f9fca6514044f505a245a652285ca7599acb2fc23de129b608548c10c046b9f5dab91f985d49d7ca15f193b8df01bc7649ef5d7e729115dfe51'
